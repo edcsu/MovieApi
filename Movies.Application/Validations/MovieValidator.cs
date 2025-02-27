ea2ab1446172e70d5d125ffce9961ecba71a13b8
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-27 13:51:59 +0300

    feat: add movie validator

---
+++ Movies.Application/Validations/MovieValidator.cs
@@ -0,0 +1,42 @@
+using FluentValidation;
+using Movies.Application.Models;
+using Movies.Application.Services;
+
+namespace Movies.Application.Validations;
+
+public class MovieValidator : AbstractValidator<Movie>
+{
+    private readonly IMovieService _movieService;
+    public MovieValidator(IMovieService movieService)
+    {
+        _movieService = movieService;
+        
+        RuleFor(m => m.Id)
+            .NotEmpty();
+        
+        RuleFor(m => m.Genres)
+            .NotEmpty();
+        
+        RuleFor(m => m.Title)
+            .NotEmpty();
+        
+        RuleFor(m => m.YearOfRelease)
+            .LessThanOrEqualTo(DateTime.UtcNow.Year);
+        
+        RuleFor(m => m.Slug)
+            .MustAsync(ValidateSlug)
+            .WithMessage("This movie already exists in the system");
+
+    }
+
+    private async Task<bool> ValidateSlug(Movie movie, string slug, CancellationToken token = default)
+    {
+        var existingMovie = await _movieService.GetBySlugAsync(slug);
+
+        if (existingMovie is not null)
+        {
+            return existingMovie.Id == movie.Id;
+        }
+        return existingMovie is null;
+    }
+}
