22480b47853d3f94352b1bee00465864c3d2e810
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-27 17:15:42 +0300

    feat: update movie repository logic

--- Movies.Application/Repositories/IMovieRepository.cs
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -8,11 +8,11 @@ public interface IMovieRepository
     
     Task<Movie?> GetByIdAsync(Guid id, Guid? userId = null, CancellationToken cancellationToken = default);
     
-    Task<Movie?> GetBySlugAsync(string slug, Guid? userId = null, CancellationToken cancellationToken = default);
+    Task<Movie?> GetBySlugAsync(string slug, Guid? userId = null, CancellationToken token = default);
     
-    Task<IEnumerable<Movie>> GetAllAsync(Guid? userId = null, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Movie>> GetAllAsync(Guid? userId = null, CancellationToken token = default);
     
-    Task<bool> UpdateAsync(Movie movie, Guid? userId = null, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(Movie movie, CancellationToken token = default);
     
     Task<bool> DeleteByIdAsync(Guid id, CancellationToken cancellationToken = default);
     
