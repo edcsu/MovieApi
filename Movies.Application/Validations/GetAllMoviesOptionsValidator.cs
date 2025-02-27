1c7964a1c42538136b9d748aaa3eda072920d7a8
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-27 19:42:12 +0300

    feat: add options validator

---
+++ Movies.Application/Validations/GetAllMoviesOptionsValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+using Movies.Application.Models;
+
+namespace Movies.Application.Validations;
+
+public class GetAllMoviesOptionsValidator : AbstractValidator<GetAllMoviesOptions>
+{
+    public GetAllMoviesOptionsValidator()
+    {
+        RuleFor(x => x.YearOfRelease)
+            .LessThanOrEqualTo(DateTime.UtcNow.Year);
+    }
+}
