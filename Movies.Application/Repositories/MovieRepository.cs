5077e2a53b5d59f012a0589b543ff9e3f97b6dbc
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-27 13:06:11 +0300

    feat: update movie in db

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -108,9 +108,30 @@ public class MovieRepository : IMovieRepository
         );
     }
 
-    public Task<bool> UpdateAsync(Movie movie)
+    public async Task<bool> UpdateAsync(Movie movie)
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.GetConnectionAsync();
+        using var transaction = connection.BeginTransaction();
+        
+        var genres = await connection.QueryAsync<string>(
+            new CommandDefinition("""
+                  delete from genres where movieId = @id
+                  """, new { id = movie.Id }));
+        
+        foreach (var genre in movie.Genres)
+        {
+            await connection.ExecuteAsync(new CommandDefinition("""
+                    insert into genres (movieId, name)
+                    values (@MovieId, @Name)
+                    """, new { MovieId = movie.Id, Name = genre}));
+        }
+        
+        var result = await connection.ExecuteAsync(new CommandDefinition("""
+             update movies set slug = @Slug, title = @Title, yearofrelease = @YearOfRelease)
+             where id = @Id              
+             """, movie));
+        transaction.Commit();
+        return result > 0;
     }
 
     public Task<bool> DeleteByIdAsync(Guid id)
