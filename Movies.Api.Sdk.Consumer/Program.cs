2b575f7432dc0772e5aed3423656a2ed40f99524
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-28 12:41:30 +0300

    feat: add consumer logic

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+using Movies.Api.Sdk;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:7078");
+
+var movie = await moviesApi.GetMovieAsync("nick-the-grrek-2022");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
