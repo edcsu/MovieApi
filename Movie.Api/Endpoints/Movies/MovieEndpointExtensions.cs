1e6468901f0d9a339f3511c255724b72fa9ca921
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-28 13:37:06 +0300

    feat: add update movie minimal endpoint

--- Movie.Api/Endpoints/Movies/MovieEndpointExtensions.cs
+++ Movie.Api/Endpoints/Movies/MovieEndpointExtensions.cs
@@ -5,6 +5,9 @@ public static class MovieEndpointExtensions
     public static IEndpointRouteBuilder MapMovieEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapGetMovie();
+        app.MapCreateMovie();
+        app.MapGetAllMovies();
+
         return app;
     }
 }
