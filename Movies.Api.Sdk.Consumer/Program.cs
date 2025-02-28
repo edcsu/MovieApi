0e5f18056d7349caadb1b54a64830711702cc0c0
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-28 12:52:10 +0300

    feat: finalise sdk consumer

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,11 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
+using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("https://localhost:7078");
+// var moviesApi = RestService.For<IMoviesApi>("https://localhost:7078");
+//
+// var movie = await moviesApi.GetMovieAsync("nick-the-grrek-2022");
 
-var movie = await moviesApi.GetMovieAsync("nick-the-grrek-2022");
+// Console.WriteLine(JsonSerializer.Serialize(movie));
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+
+var services = new ServiceCollection();
+
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s => new RefitSettings
+    {
+        AuthorizationHeaderValueGetter = async (_, token) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync()
+    })
+    .ConfigureHttpClient(x =>
+        x.BaseAddress = new Uri("https://localhost:5001"));
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("nick-the-greek-2022");
+
+var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+{
+    Title = "Spiderman 2",
+    YearOfRelease = 2002,
+    Genres = ["Action"]
+});
+
+await moviesApi.UpdateMovieAsync(newMovie.Id, new UpdateMovieRequest()
+{
+    Title = "Spiderman 2",
+    YearOfRelease = 2002,
+    Genres = ["Action", "Adventure"]
+});
+
+await moviesApi.DeleteMovieAsync(newMovie.Id);
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
+
