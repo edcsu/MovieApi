101ebcc649b732069359986465f9a5a4420ebe18
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-28 13:46:08 +0300

    feat: add delete rate endpoint

--- Movie.Api/Endpoints/Ratings/RatingEndpointExtensions.cs
+++ Movie.Api/Endpoints/Ratings/RatingEndpointExtensions.cs
@@ -5,7 +5,8 @@ public static class RatingEndpointExtensions
     public static IEndpointRouteBuilder MapRatingEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapRateMovie();
-        
+        app.MapDeleteRating();
+
         return app;
     }
 }
